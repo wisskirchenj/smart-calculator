abc4101b99f38e1bdbe7f95eb3a3ddaf07d681bd
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-09-04 22:45:53 +0200

    complete stage 3

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -3,7 +3,9 @@ class Calculator:
     def run(self):
         line = input()
         while line != '/exit':
-            if line:
+            if line == '/help':
+                print('The program calculates the sum of numbers')
+            elif line:
                 print(sum([int(token) for token in line.split()]))
             line = input()
         print('bye')
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -10,11 +10,13 @@ class TestCalculator(unittest.TestCase):
     @patch('builtins.input')
     @patch('sys.stdout', new_callable=StringIO)
     def test_example1(self, mock_stdout: StringIO, mock_input: MagicMock):
-        mock_input.side_effect = ['5 8', '1 -1', '-5 12345', '7', '', '/exit']
+        mock_input.side_effect = ['5 8 2 -13', '', '1 -1', '-5 12345', '7', '', '/help', '/exit']
         Calculator().run()
-        self.assertEqual(1, mock_stdout.getvalue().count('13'))
-        self.assertEqual(1, mock_stdout.getvalue().count('\n0\n'))
-        self.assertEqual(1, mock_stdout.getvalue().count('12340'))
-        self.assertEqual(1, mock_stdout.getvalue().count('7'))
-        self.assertEqual(1, mock_stdout.getvalue().count('bye'))
-        self.assertEqual(5, len(mock_stdout.getvalue().split()))
+        lines = mock_stdout.getvalue().splitlines()
+        self.assertEqual(6, len(lines))
+        self.assertEqual('2', lines[0])
+        self.assertEqual('0', lines[1])
+        self.assertEqual('12340', lines[2])
+        self.assertEqual('7', lines[3])
+        self.assertEqual('The program calculates the sum of numbers', lines[4])
+        self.assertEqual('bye', lines[5])

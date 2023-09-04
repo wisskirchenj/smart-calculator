0e6e3423d2e55d09ed750a390ac59703f8e4e3ad
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-09-04 23:34:06 +0200

    complete stage 4

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -4,11 +4,23 @@ class Calculator:
         line = input()
         while line != '/exit':
             if line == '/help':
-                print('The program calculates the sum of numbers')
+                print('The program simplifies a term of sums and differences')
             elif line:
-                print(sum([int(token) for token in line.split()]))
+                print(self.simplify_term(line.split()))
             line = input()
         print('bye')
+
+    def simplify_term(self, tokens):
+        result = tokens[0]
+        for i in range(1, len(tokens), 2):
+            result = self.calc(result, self.resolve_operator(tokens[i]), tokens[i + 1])
+        return result
+
+    def calc(self, x, op, y):
+        return eval(f'{x} {op} {y}')
+
+    def resolve_operator(self, operator_token):
+        return '-' if operator_token.count('-') % 2 else '+'
 
 
 if __name__ == '__main__':
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -10,13 +10,15 @@ class TestCalculator(unittest.TestCase):
     @patch('builtins.input')
     @patch('sys.stdout', new_callable=StringIO)
     def test_example1(self, mock_stdout: StringIO, mock_input: MagicMock):
-        mock_input.side_effect = ['5 8 2 -13', '', '1 -1', '-5 12345', '7', '', '/help', '/exit']
+        mock_input.side_effect = ['-2 + 4 - 5 + 6', '', '1 -- -1', '9 +++ 10 -- 8', '7', '', '/help',
+                                  '14       ---   12', '/exit']
         Calculator().run()
         lines = mock_stdout.getvalue().splitlines()
-        self.assertEqual(6, len(lines))
-        self.assertEqual('2', lines[0])
+        self.assertEqual(7, len(lines))
+        self.assertEqual('3', lines[0])
         self.assertEqual('0', lines[1])
-        self.assertEqual('12340', lines[2])
+        self.assertEqual('27', lines[2])
         self.assertEqual('7', lines[3])
-        self.assertEqual('The program calculates the sum of numbers', lines[4])
-        self.assertEqual('bye', lines[5])
+        self.assertEqual('The program simplifies a term of sums and differences', lines[4])
+        self.assertEqual('2', lines[5])
+        self.assertEqual('bye', lines[6])

309bbc8722a5ae09a4b97334568051979b805c9b
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-09-12 18:09:07 +0200

    complete stage 5 with tests

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,25 +1,39 @@
+import re
+
+
 class Calculator:
 
     def run(self):
         line = input()
         while line != '/exit':
-            if line == '/help':
-                print('The program simplifies a term of sums and differences')
+            if line.startswith('/'):
+                self.execute_command(line[1:])
             elif line:
-                print(self.simplify_term(line.split()))
+                self.simplify_term(line.split())
             line = input()
-        print('bye')
+
+    @staticmethod
+    def execute_command(command):
+        if command == 'help':
+            print('The program simplifies a term of sums and differences')
+        else:
+            print('Unknown command')
 
     def simplify_term(self, tokens):
-        result = tokens[0]
-        for i in range(1, len(tokens), 2):
-            result = self.calc(result, self.resolve_operator(tokens[i]), tokens[i + 1])
-        return result
+        try:
+            result = int(tokens[0])
+            for i in range(1, len(tokens), 2):
+                result = self.calc(result, self.resolve_operator(tokens[i]), int(tokens[i + 1]))
+            print(result)
+        except (IndexError, ValueError):
+            print('Invalid expression')
 
     def calc(self, x, op, y):
         return eval(f'{x} {op} {y}')
 
     def resolve_operator(self, operator_token):
+        if not re.match('[-+]+', operator_token):
+            raise ValueError
         return '-' if operator_token.count('-') % 2 else '+'
 
 
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -10,15 +10,21 @@ class TestCalculator(unittest.TestCase):
     @patch('builtins.input')
     @patch('sys.stdout', new_callable=StringIO)
     def test_example1(self, mock_stdout: StringIO, mock_input: MagicMock):
-        mock_input.side_effect = ['-2 + 4 - 5 + 6', '', '1 -- -1', '9 +++ 10 -- 8', '7', '', '/help',
-                                  '14       ---   12', '/exit']
+        mock_input.side_effect = ['-2 + 4 - 5 + 6', '', '1 -- -1', '9 +++ 10 -- 8', '+7', '', '/help',
+                                  '14       ---   12', '/', '/go', '23+', '23 +', '23 12', 'a + b', '2 * 3', '/exit']
         Calculator().run()
         lines = mock_stdout.getvalue().splitlines()
-        self.assertEqual(7, len(lines))
+        self.assertEqual(13, len(lines))
         self.assertEqual('3', lines[0])
         self.assertEqual('0', lines[1])
         self.assertEqual('27', lines[2])
         self.assertEqual('7', lines[3])
         self.assertEqual('The program simplifies a term of sums and differences', lines[4])
         self.assertEqual('2', lines[5])
-        self.assertEqual('bye', lines[6])
+        self.assertEqual('Unknown command', lines[6])
+        self.assertEqual('Unknown command', lines[7])
+        self.assertEqual('Invalid expression', lines[8])
+        self.assertEqual('Invalid expression', lines[9])
+        self.assertEqual('Invalid expression', lines[10])
+        self.assertEqual('Invalid expression', lines[11])
+        self.assertEqual('Invalid expression', lines[12])
